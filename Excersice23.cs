5cef538d0c093c26e6e70532dd2baebac9fa50d1
DANSRM02 <danielmercenaries417@gmail.com>
2026-08-18 19:37:45 -0500

    refactor: align project to keep it simple

---
+++ Excersice23.cs
@@ -0,0 +1,19 @@
+public class Exersice23
+{
+  public static void CalculateRabbitPopulation(int today_population, int yesterday_population)
+  {
+    int counter = 0;
+    int tomorrow_population = 0;
+
+    while (tomorrow_population < 70000)
+    {
+      tomorrow_population = today_population + yesterday_population;
+      yesterday_population = today_population;
+      today_population = tomorrow_population;
+      counter++;
+    }
+
+    Console.WriteLine($"day to export rabbit remaning {counter}");
+  }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Which exercise do you want to run? (1-100)");
+string input = Console.ReadLine() ?? "empty";
 
-static string CalculateRabbitPopulation(int today_population, int yesterday_population)
+switch (input)
 {
-  int counter = 0;
-  int tomorrow_population = 0;
-
-  while (tomorrow_population < 70000)
-  {
-    tomorrow_population = today_population + yesterday_population;
-    yesterday_population = today_population;
-    today_population = tomorrow_population;
-    counter++;
-  }
-
-  return $"day to export rabbit remaning {counter}";
+    case "1":
+        Exersice23.CalculateRabbitPopulation(15000, 7000);
+        break;
+    default:
+        Console.WriteLine("Exercise not found.");
+        break;
 }
-
-Console.WriteLine(CalculateRabbitPopulation(15000, 25000));
