d4a46ad3798e479db08c155335494fd3aa294f67
DANSRM02 <danielmercenaries417@gmail.com>
2026-08-19 20:47:39 -0500

    fix: update indent spaces from 4 to 2

--- ExercisesPartOne/Exercise14.cs
+++ ExercisesPartOne/Exercise14.cs
@@ -2,22 +2,22 @@ namespace ExercisesPartOne;
 
 class Exercise14
 {
-    public static void CalculateFibonacci()
+  public static void CalculateFibonacci()
+  {
+    int nextValue = 0;
+    int currentValue = 1;
+    int prevValue = 0;
+
+    Console.WriteLine("Enter a number to see the fibonacci sequence");
+    int.TryParse(Console.ReadLine(), out int iterator);
+
+    for (int i = 0; i <= iterator; i++)
     {
-        int nextValue = 0;
-        int currentValue = 1;
-        int prevValue = 0;
+      Console.WriteLine($"{prevValue}");
 
-        Console.WriteLine("Enter a number to see the fibonacci sequence");
-        int iterator = int.Parse(Console.ReadLine());
-
-        for (int i = 0; i <= iterator; i++)
-        {
-            Console.WriteLine($"{prevValue}");
-
-            nextValue = currentValue + prevValue;
-            prevValue = currentValue;
-            currentValue = nextValue;
-        }
+      nextValue = currentValue + prevValue;
+      prevValue = currentValue;
+      currentValue = nextValue;
     }
+  }
 }
