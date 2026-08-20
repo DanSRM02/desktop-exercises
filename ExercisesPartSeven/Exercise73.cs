c1bbb160a2065bd00af90df2c58a9c5f3b65f2b1
DANSRM02 <danielmercenaries417@gmail.com>
2026-08-19 20:47:22 -0500

    feat: exercise 73 factorial of a number

--- ExercisesPartSeven/Exercise73.cs
+++ ExercisesPartSeven/Exercise73.cs
@@ -2,8 +2,18 @@ namespace ExercisesPartSeven;
 
 class Exercise73
 {
-    public static void CalculateFactorialNumber()
-    {
+  public static void CalculateFactorialNumber()
+  {
+    Console.WriteLine("Enter a number to know its factorial result");
+    int.TryParse(Console.ReadLine(), out int iterator);
 
+    int accumulator = 1;
+
+    for (int i = 1; i <= iterator; i++)
+    {
+      accumulator *= i;
     }
+
+    Console.WriteLine($"Result:{accumulator} Number:{iterator}");
+  }
 }
